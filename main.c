b1e589437caa75de124c299672fac0dbaab659f0
Julien <julien.gustin@student.uliege.be>
2019-10-17 21:41:47 +0200

    Amélioration rapport ( debut complexité )

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char** argv)
 {
    //--------------------------- PARSE COMMAND LINE -------------------------
     unsigned seed = (unsigned) time(NULL);
-    size_t sizeArray = 10;
+    size_t sizeArray = 100;
     bool useBlocks = false;
     size_t sizeBlocks = 5;
     int argi = 1; // Ideally this should be a size_t (historical reasons)
