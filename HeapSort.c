1d650ddf2425cc70629d3b48e147c768be660d9f
mathi1905 <mathiascarlisi@hotmail.fr>
2019-10-22 14:41:31 +0200

    Commentaires heapsort

--- HeapSort.c
+++ HeapSort.c
@@ -9,11 +9,59 @@
 #include <limits.h>
 #include "Sort.h"
 
+/* ------------------------------------------------------------------------- *
+ * Echange les éléments du tableau array[i] avec array[j] tel que, array[i] <=> array[j]
+ *
+ * PARAMETRES
+ * array        Tableau d'entier
+ * i            Indice du premier éléments
+ * j            Indice du second éléments
+ * ------------------------------------------------------------------------- */
 static void swap(int *array, size_t i, size_t j);
+
+/* ------------------------------------------------------------------------- *
+ * Retourne le fils gauche du noeud i de l'arbre binaire
+ *
+ * PARAMETRES
+ * i       Le noeud i de l'arbre binaire
+ * ------------------------------------------------------------------------- */
 static size_t Left(size_t i);
+
+/* ------------------------------------------------------------------------- *
+ * Retourne le fils droit du noeud i de l'arbre binaire
+ *
+ * PARAMETRES
+ * i       Le noeud i de l'arbre binaire
+ * ------------------------------------------------------------------------- */
 static size_t Right(size_t i);
+
+/* ------------------------------------------------------------------------- *
+ * Rétablit la propriété des tas(pour tout i, en tenant compte du fait que les
+ * sous-arbres de droite et de gauche sont des tas
+ *
+ * PARAMETRES
+ * array        Tableau d'entier
+ * heap_size     La taille du tas
+ * ------------------------------------------------------------------------- */
 static void Max_Heapify(int *array, size_t i, size_t heap_size);
+
+/* ------------------------------------------------------------------------- *
+ * Construit un tas (arbre binaire)à partir du tableau à trier
+ *
+ * PARAMETRES
+ * array        Tableau d'entier
+ * length       La taille du tableau
+ * ------------------------------------------------------------------------- */
 static void Build_Max_Heap(int *array, size_t length);
+
+/* ------------------------------------------------------------------------- *
+ * Trie un tableau d'entier.
+ *
+ * PARAMETRES
+ * array        Le tableau à trier
+ * length       La taille du tableau
+ * ------------------------------------------------------------------------- */
+static void HeapSort(int *array, size_t length);
 
 static size_t Left(size_t i){
   return 2*i;
@@ -68,7 +116,5 @@ static void HeapSort(int *array, size_t length){
 }
 
 void sort(int* array, size_t length){
-
   HeapSort(array, length-1);
-
 }
